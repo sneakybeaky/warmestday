10bab12937a8142a68aa0df360e71452fda82552
Jon Barber <jon.barber@acm.org>
2023-02-22 13:10:00 +0000

    test: When many days share max temp and humidity return the first

--- weather_test.go
+++ weather_test.go
@@ -116,6 +116,44 @@ func TestWarmestDayChosenInFirstSevenOnly(t *testing.T) {
 	}
 }
 
+func TestFirstOfManyChosenWhenTheyHaveSameTempAndHumidity(t *testing.T) {
+
+	t.Parallel()
+
+	wantDay := "2020-01-20"
+
+	var f forecastfunc = func(_, _ float64) (forecast.Forecast, error) {
+		return forecast.Forecast{
+			Timezone: "Europe/London",
+
+			Days: []forecast.Day{{
+				Date:            mustTime(t, wantDay),
+				MaximumTemp:     9,
+				HumidityPercent: 20,
+			}, {
+				Date:            mustTime(t, wantDay),
+				MaximumTemp:     10,
+				HumidityPercent: 20,
+			}, {
+				Date:            mustTime(t, "2020-01-21"),
+				MaximumTemp:     10,
+				HumidityPercent: 20,
+			}},
+		}, nil
+	}
+
+	w := warmestday.NewWeather(f)
+	got, err := w.Summary(0, 0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.WarmestDay != wantDay {
+		t.Fatalf("Expected %q but got %q", wantDay, got.WarmestDay)
+	}
+}
+
 func mustTime(t *testing.T, value string) time.Time {
 	t.Helper()
 	parsed, err := time.Parse("2006-01-02", value)
